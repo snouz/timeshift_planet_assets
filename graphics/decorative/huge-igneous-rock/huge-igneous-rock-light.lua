6e50cc54f9790542f22a67b922865ae8e09f8723
snouz <snooz-x@hotmail.com>
2025-09-25 23:05:51 +0200

    igneous rock graphics

---
+++ graphics/decorative/huge-igneous-rock/huge-igneous-rock-light.lua
@@ -0,0 +1,9 @@
+return
+{
+  width = 1,
+  height = 1,
+  shift = {0,0},
+  line_length = 5,
+  --draw_as_glow = true,
+  blend_mode = "additive",
+}
---
+++ graphics/decorative/huge-igneous-rock/huge-igneous-rock.lua
@@ -0,0 +1,7 @@
+return
+{
+  width = 320,
+  height = 512,
+  shift = {0,0},
+  line_length = 5,
+}
